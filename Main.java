3e923c21f61e73df8968bd4bb5c72b38ac0a10d6
unknown <daavwid2001@gmail.com>
2024-05-04 12:08:59 +0200

    zad3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +10,11 @@ public class Main {
             Person person = new Person("Lord Voldemort", 71);
             System.out.println("Imie i nazwisko: " + person.getName() + ", Wiek: " + person.getAge());
 
+            int sum = MathUtils.add(50, 21);
+            System.out.println("Sumą 50 i 21 jest: " + sum);
+
             Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("Witamy w śmierciożercach " + person.getName() + "!");
+            messenger.sendMessage("Witamy w śmierciożercach " + person.getName() + "! Sumą 50 i 21 jest: " + sum);
         } catch (InvalidAgeException e) {
             System.err.println(e.getMessage());
         }
