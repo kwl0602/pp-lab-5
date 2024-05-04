f9d0ba8a4b9977991396c6ad45c1b861e45c403b
unknown <daavwid2001@gmail.com>
2024-05-04 11:57:01 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Person person = new Person("Lord Voldemort", 71);
-            System.out.println("Imie i naziwsko: " + person.getName() + ", wiek: " + person.getAge());
+            System.out.println("Imie i nazwisko: " + person.getName() + ", Wiek: " + person.getAge());
+
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("Witamy w śmierciożercach " + person.getName() + "!");
         } catch (InvalidAgeException e) {
             System.err.println(e.getMessage());
         }
