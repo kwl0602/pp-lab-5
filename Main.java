bf41fecf5fa0db1d9cd6f5d9cebba6b7d377d4a8
unknown <daavwid2001@gmail.com>
2024-05-04 12:55:30 +0200

    zad4

--- Main.java
+++ Main.java
@@ -6,17 +6,27 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        Person[] people = new Person[5];
+        final int b = 10;
+
+        Messenger messenger = new EmailMessenger();
+
         try {
-            Person person = new Person("Lord Voldemort", 71);
-            System.out.println("Imie i nazwisko: " + person.getName() + ", Wiek: " + person.getAge());
+            people[0] = new Person("Peter Pettigrew", 38);
+            people[1] = new Person("Lord Voldemort", 71);
+            people[2] = new Person("Regulus Black", 18);
+            people[3] = new Person("Bellatriks Lestrange", 47);
+            people[4] = new Person("Severus Snape", 38);
 
-            int sum = MathUtils.add(50, 21);
-            System.out.println("Sumą 50 i 21 jest: " + sum);
-
-            Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("Witamy w śmierciożercach " + person.getName() + "! Sumą 50 i 21 jest: " + sum);
+            for (Person person : people) {
+                int agePlusB = MathUtils.add(person.getAge(), b);
+                String message = "Witaj śmierciożerco " + person.getName() + "! Twój wiek + 10 to: " + agePlusB;
+                messenger.sendMessage(message);
+            }
         } catch (InvalidAgeException e) {
             System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Błąd: " + e.getMessage());
         }
     }
 }
